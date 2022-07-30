8a2b4b49006a64a6f67a5742f10ecbe4a17333f2
Mun Hin <obh555@gmail.com>
2022-07-30 13:51:17 +0800

    bot will always take center

--- js/app/bot.js
+++ js/app/bot.js
@@ -1,15 +1,15 @@
-import {delayExecution, selectOne} from '../../utils/index.js';
-import board from './board.js';
+import { delayExecution, selectOne } from "../../utils/index.js";
+import board from "./board.js";
 
 const bot = {
   checkMoves(symbol, combinations) {
     const moves = [];
     for (const combination of combinations) {
-      const count = {filledCells: 0, emptyCells: 0};
-      for (const {symbol: content} of combination) {
+      const count = { filledCells: 0, emptyCells: 0 };
+      for (const { symbol: content } of combination) {
         if (content === symbol) {
           count.filledCells++;
-        } else if (content === '') {
+        } else if (content === "") {
           count.emptyCells++;
         }
         if (count.filledCells === 2 && count.emptyCells === 1) {
@@ -22,33 +22,38 @@ const bot = {
 
   chooseMove(moves) {
     const move = selectOne(moves);
-    for (const {symbol, index} of move) {
+    for (const { symbol, index } of move) {
       if (!symbol) return index;
     }
   },
 
   randomMove(currentBoard) {
     const indexes = Array.from(currentBoard).reduce(
-        (indexes, column, index) => {
-          if (!column.disabled) indexes.push(index);
-          return indexes;
-        },
-        [],
+      (indexes, column, index) => {
+        if (!column.disabled) indexes.push(index);
+        return indexes;
+      },
+      []
     );
     return selectOne(indexes);
   },
 
   async findMove(currentBoard) {
-    await delayExecution(750);
+    await delayExecution(750, currentBoard);
     const combinations = board.getCurrent(currentBoard);
-    const winningMoves = bot.checkMoves('O', combinations);
-    const blockingMoves = bot.checkMoves('X', combinations);
+    const winningMoves = bot.checkMoves("O", combinations);
+    const blockingMoves = bot.checkMoves("X", combinations);
     if (winningMoves.length > 0) {
       return currentBoard[bot.chooseMove(winningMoves)];
     }
     if (blockingMoves.length > 0) {
       return currentBoard[bot.chooseMove(blockingMoves)];
     }
+
+    if (!currentBoard[4].disabled) {
+      return currentBoard[4];
+    }
+
     return currentBoard[bot.randomMove(currentBoard)];
   },
 };
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,13 @@
-const selectOne = arr => arr[Math.floor(Math.random() * arr.length)]
-const delayExecution = duration => new Promise(resolve => setTimeout(resolve, duration))
+const selectOne = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-export { selectOne, delayExecution }
+const delayExecution = (duration, board) => {
+  board.forEach((column) => (column.style.pointerEvents = "none"));
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+      board.forEach((column) => (column.style.pointerEvents = "auto"));
+    }, duration)
+  );
+};
+
+export { selectOne, delayExecution };
