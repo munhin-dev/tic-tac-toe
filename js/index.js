48eac84d06433993014b33383aeff7a5892d87c6
obh555 <obh555@gmail.com>
2022-10-02 14:09:11 +0800

    testing

--- js/index.js
+++ js/index.js
@@ -18,4 +18,5 @@ async function handleClick(event) {
 gameBoard.forEach((column) => column.addEventListener("click", handleClick));
 overlay.addEventListener("click", game.restartGame);
 scoreBoard.addEventListener("click", game.toggleBot);
+console.log("test finished  ")
  
